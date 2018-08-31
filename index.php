ba6275e0ada5277340134eeb6e9ff67ae9778411
Chetan Thakare <chetan.thakare90@gmail.com>
2018-08-31 13:54:59 +0530

    Display Word Files on Browser
    Display Word Files on Browser

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Sample Word File</p>
+<?php 
+$src = "http://www.iiswc.org/iiswc2009/sample.doc";
+
+echo "<iframe src='http://docs.google.com/gview?url=".$src."&embedded=true' style='width:600px; height:550px;' frameborder='10'></iframe>"; ?>
+</body>
+
+</html>
